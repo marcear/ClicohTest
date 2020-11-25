cd23549aedf8331c38f85ce8f31907a311019c5f
Marcelo Arias <ariasmarcelof@gmail.com>
2020-11-25 19:48:55 -0300

    add footer icons

--- clicohtest/src/components/Footer/Footer.js
+++ clicohtest/src/components/Footer/Footer.js
@@ -2,6 +2,7 @@
 import React from "react";
 //antd
 import { Layout } from "antd";
+import { GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
 //scss
 import "./Footer.scss";
 
@@ -9,7 +10,17 @@ const { Footer: AntFooter } = Layout;
 
 const Footer = () => {
   return (
-    <AntFooter className="footer-main">Test Clicoh - Marcelo Arias</AntFooter>
+    <AntFooter className="footer-main">
+      Test Clicoh - Marcelo Arias
+      <div className="icons-list">
+        <a href="https://github.com/marcear" target="_blank">
+          <GithubOutlined />
+        </a>
+        <a href="https://www.linkedin.com/in/marceloarias2020/" target="_blank">
+          <LinkedinOutlined />
+        </a>
+      </div>
+    </AntFooter>
   );
 };
 
